cea805eeacc27256afb8606c2858812eed7a684b
Miftahul Islam <almiftahulislam@gmail.com>
2026-05-14 17:37:28 +0600

    chore: remove Google Tag Manager component from BlogLayout

--- src/app/(customer)/layout.tsx
+++ src/app/(customer)/layout.tsx
@@ -2,7 +2,6 @@ import { getPublicSettings } from "@/actions/settings";
 import Footer from "@/components/footer/footer-new";
 import { Navbar } from "@/components/navbar/navbar"
 import { CurrencyProvider } from "@/components/providers/currency-provider"
-import { GoogleTagManager } from "@next/third-parties/google";
 
 export const dynamic = 'force-dynamic'
 
@@ -18,12 +17,10 @@ export default async function BlogLayout({
             <Navbar storeName={settings.store_name} />
             <main className="relative min-h-screen bg-background">
                 <div className="mx-auto w-full">
-                    <GoogleTagManager gtmId="GTM-NJSH52CZ" />
                     {children}
                 </div>
             </main>
             <Footer />
-            <GoogleTagManager gtmId="GTM-NJSH52CZ" />
         </CurrencyProvider>
     )
 }
